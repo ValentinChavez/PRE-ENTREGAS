8e0abe70cef9b6e63770b960b5456a2612978a58
valentinchavez <valeonchavez@gmail.com>
2022-09-22 15:01:52 -0300

    2da Preentrega

---
+++ stock.js
@@ -0,0 +1,14 @@
+let stockProductos = [
+    {id: 1, nombre: "Guitarra Acustica Epiphone", tipo: "cuerda", cantidad: 1, desc: "Recomendado para tocar Folklore, Baladas y Country-Rock", precio: 30000,  img: './img/guitarra1.jpg'},
+    {id: 2, nombre: "Bajo Electrico Whazz", tipo: "cuerda", cantidad: 1, desc: "Ideales para principiantes en el bajo", precio: 15000, img: './img/bajo.jpg'},
+    {id: 3, nombre: "Clarinete Coda", tipo: "viento", cantidad: 1, desc: "Muy bueno para aquellos que ya tienen un conocimiento previo en instrumentos de viento", precio: 32000, img: './img/clarinete.jpg'},
+    {id: 4, nombre: "Banjo Parquer", tipo: "cuerda", cantidad: 1, desc: "Perfecto para tocar musica Folk o Country-Rock", precio: 18000, img: './img/banjo.jpg'},
+    {id: 5, nombre: "Guitarra Elecrica Epiphone", tipo: "cuerda", cantidad: 1, desc: "Muy buena guitarra calidad precio", precio: 32000, img: './img/guitarra2.jpg'},
+    {id: 6, nombre: "Trompeta Parquer", tipo: "viento", cantidad: 1, desc: "Muy bueno para cualquier tipo de musica", precio: 20000, img: './img/trompeta.jpg'},
+    {id: 7, nombre: "Violin Parquer", tipo: "cuerda", cantidad: 1, desc: "Muy bueno para musica clasica", precio: 16000, img: './img/violin.jpg'},
+    {id: 8, nombre: "Tuba Parquer", tipo: "viento", cantidad: 1, desc: "Perfecto para aquellos que ya tienen un conocimieto basico del instrumento", precio: 40000, img: './img/tuba.jpg'},
+    {id: 9, nombre: "Ukelele Parquer", tipo: "cuerda", cantidad: 1, desc: "Ideal para introducirce en el mundo de la musica", precio: 12000, img: './img/uke.jpg'},
+    {id: 10, nombre: "Guitarra Electrica Squier", tipo: "cuerda", cantidad: 1, desc: "Muy buena para tocar Rock, Reggae, y basicamente cualquier cosa que se te ocurra", precio: 38000, img: './img/guitarra3.jpg'},
+    {id: 11, nombre: "Saxofon Soprano", tipo: "viento", cantidad: 1, desc: "Un instrumento eleganrte para tocar lo que mas te guste", precio: 36000, img: './img/saxo.jpg'},
+    {id: 12, nombre: "Violoncello de concierto", tipo: "cuerda", cantidad: 1, desc: "Ideal para tocar musica clasica", precio: 65000, img: './img/chelo.jpg'},
+]
